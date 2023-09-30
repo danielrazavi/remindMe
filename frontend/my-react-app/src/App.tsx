40454b91edc429c7255c992458c7fda5afdbdc7a
Daniel R <danraz1376@gmail.com>
2023-09-30 16:37:46 -0400

    Added Material UI library to the project.

--- frontend/my-react-app/src/App.tsx
+++ frontend/my-react-app/src/App.tsx
@@ -1,5 +1,14 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
 function App() {
-  return <div className="App">Hello Dan!</div>;
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Hello Dan!
+      </Button>
+    </div>
+  );
 }
 
 export default App;
