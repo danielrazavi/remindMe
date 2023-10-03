a3240e7c665eb509e959e6c8a08db4c787c3ce10
Daniel R <danraz1376@gmail.com>
2023-10-02 20:35:54 -0400

    Dummy Data, Card updated,

--- frontend/my-react-app/src/App.tsx
+++ frontend/my-react-app/src/App.tsx
@@ -9,7 +9,41 @@ import Stack from "@mui/material/Stack";
 import OpenIconSpeedDial from "./components/SpeedDial";
 import ReminderCards from "./components/Card";
 
+// Define a function to return the dummy data
+function getDummyData() {
+  return [
+    {
+      id: 1,
+      title: "Buy Groceries",
+      description: "Milk, eggs, bread, and fruits",
+      dueDate: "2023-10-10T12:00:00Z",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Meeting with Client",
+      description: "Discuss project requirements",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Pay Utility Bills",
+      description: "Electricity and water bills",
+      dueDate: "2023-10-20T10:00:00Z",
+      completed: true,
+    },
+    {
+      id: 4,
+      title: "Finish Homework",
+      description: "Complete math assignment",
+      dueDate: "2023-10-12T18:00:00Z",
+      completed: false,
+    },
+  ];
+}
+
 function App() {
+  const reminders = getDummyData();
   return (
     <React.Fragment>
       <CssBaseline />
@@ -19,7 +53,16 @@ function App() {
       <Box height={20} />
       <Container maxWidth="sm">
         <Stack spacing={2}>
-          <ReminderCards />
+          {reminders.map((reminder) =>
+            !reminder.completed ? (
+              <ReminderCards
+                key={reminder.id}
+                title={reminder.title}
+                description={reminder.description}
+                dueDate={reminder.dueDate ? new Date(reminder.dueDate) : null}
+                completed={reminder.completed}></ReminderCards>
+            ) : null
+          )}
         </Stack>
         <Box height={20} />
 
--- frontend/my-react-app/src/components/Card.tsx
+++ frontend/my-react-app/src/components/Card.tsx
@@ -4,27 +4,38 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 
-export default function ReminderCards() {
+interface ReminderCardProps {
+  title: string;
+  description: string;
+  dueDate: Date | null;
+  completed: boolean;
+}
+
+export default function ReminderCards({
+  title,
+  description,
+  dueDate,
+  completed,
+}: ReminderCardProps) {
   return (
     <Card variant="outlined">
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
         <Typography variant="h5" component="div">
-          benevolent
+          {title}
         </Typography>
+
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
+          {description}
         </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+
+        {dueDate ? (
+          <Typography variant="body2">
+            Due {dueDate.toDateString()} at {dueDate.toLocaleTimeString()}
+          </Typography>
+        ) : null}
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <Button size="small">More Details</Button>
       </CardActions>
     </Card>
   );
